5835a3ab7bc61df007ddb694a6f6bf7bfc56ec21
joshzbarta <joshzbarta@gmail.com>
2020-09-30 01:28:03 -0500

    REF: Rename UtilityModels to utilityModels

---
+++ wear/src/main/java/com/eveningoutpost/dexdrip/utilityModels/CompatibleApps.java
@@ -0,0 +1,7 @@
+package com.eveningoutpost.dexdrip.utilityModels;
+
+public class CompatibleApps {
+
+    // stub
+        public static final String EXTERNAL_ALG_PACKAGES = "EXTERNAL_ALG_PACKAGES";
+}
