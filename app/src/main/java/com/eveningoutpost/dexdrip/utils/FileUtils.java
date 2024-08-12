408ac7b5241e8d7e8554cec908d62f5f8b8337d6
joshzbarta <joshzbarta@gmail.com>
2024-08-12 03:43:19 -0500

    Delombok FileUtils.java

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/FileUtils.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/FileUtils.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import lombok.val;
-
 import com.eveningoutpost.dexdrip.xdrip;
 import com.eveningoutpost.dexdrip.data.UserError;
 
@@ -64,7 +62,7 @@ public class FileUtils {
     }
 
     public static byte[] readFromFile(final String TAG, final String fileName) {
-        val fl = new File(fileName);
+        File fl = new File(fileName);
         return readFromFile(TAG, fl);
     }
 
@@ -73,9 +71,9 @@ public class FileUtils {
             UserError.Log.e(TAG, "File does not exist " + fl.getName());
             return null;
         }
-        val data = new byte[(int) fl.length()];
+        byte[] data = new byte[(int) fl.length()];
         try {
-            val f = new FileInputStream(fl);
+            FileInputStream f = new FileInputStream(fl);
             int size = f.read(data);
             f.close();
             if (size != data.length) {
